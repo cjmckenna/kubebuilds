87fede795d9cfa6710331a2c2e227deb631aae68
cjmckenna <kb1sln@gmail.com>
2023-01-11 17:53:04 -0500

    Add files via upload

--- calicoinstall.sh
+++ calicoinstall.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 kubectl create -f https://docs.projectcalico.org/manifests/tigera-operator.yaml
 kubectl create -f https://docs.projectcalico.org/manifests/custom-resources.yaml
+kubectl wait pod --all  --all-namespaces --for=condition=ready
+echo "Calico install completed"
