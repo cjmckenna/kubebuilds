0f5e053b90a107b2a12710dd46299ce94aa2cabd
cjmckenna <kb1sln@gmail.com>
2023-01-12 13:59:13 -0500

    Add files via upload

---
+++ metalLBinstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+MetalLB_RTAG=$(curl -s https://api.github.com/repos/metallb/metallb/releases/latest|grep tag_name|cut -d '"' -f 4|sed 's/v//')
+echo $MetalLB_RTAG
+mkdir ~/metallb
+cd ~/metallb
+wget https://raw.githubusercontent.com/metallb/metallb/v$MetalLB_RTAG/config/manifests/metallb-native.yaml
+kubectl apply -f metallb-native.yaml
+kubectl wait pod --all  -n metallb-system --for=condition=ready
+kubectl apply -f  ~/metallb/ipaddress_pools.yaml
+kubectl describe ipaddresspools.metallb.io production -n metallb-system
