da4fcd279e00dcad9560017fe359fa4eb608d606
cjmckenna <kb1sln@gmail.com>
2023-01-13 14:20:36 -0500

    Add files via upload

---
+++ testjoincmd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+joincommand=$(base64 joincommand.txt | tr -d \\n)
+
+shavalue=$(curl -X GET https://api.github.com/repos/cjmckenna/kubebuilds/contents/joincommand.txt | jq .sha)
+
+if [ -z "$shavalue" ]; then
+  curl -i -X PUT -H "Authorization: token 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" -d "{\"path\": \"joincommand.txt\", \"message\": \"initial commit\", \"content\": \"${joincommand}\"}" https://api.github.com/repos/cjmckenna/kubebuilds/contents/joincommand.txt
+else
+  curl -i -X PUT -H "Authorization: token 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" -d "{\"path\": \"joincommand.txt\", \"message\": \"initial commit\", \"content\": \"${joincommand}\", \"sha\": $shavalue}" https://api.github.com/repos/cjmckenna/kubebuilds/contents/joincommand.txt
+fi
