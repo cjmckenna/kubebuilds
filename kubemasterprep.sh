19d0f06ac10d63d8b4f4d40cbdca2c86fba5af56
cjmckenna <kb1sln@gmail.com>
2023-01-13 11:19:22 -0500

    Add files via upload

--- kubemasterprep.sh
+++ kubemasterprep.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Prepare server for kube install
+echo "DOWNLOADING FILES"
 curl -O https://raw.githubusercontent.com/cjmckenna/kubebuilds/main/buildkubemaster.sh && chmod 777 buildkubemaster.sh
 curl -O https://raw.githubusercontent.com/cjmckenna/kubebuilds/main/exports
 curl -O https://raw.githubusercontent.com/cjmckenna/kubebuilds/main/nfsserverbuild.sh && chmod 777 nfsserverbuild.sh
@@ -8,15 +9,21 @@ curl -O https://raw.githubusercontent.com/cjmckenna/kubebuilds/main/nfsserverbui
 curl -O https://raw.githubusercontent.com/cjmckenna/kubebuilds/main/chris_sudo
 echo medic8877 | sudo -S cp chris_sudo /etc/sudoers.d/chris_sudo
 rm chris_sudo
+
+echo "TURNING OFF SWAP"
 # turn off swap
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 sudo sed -ri '/\sswap\s/s/^#?/#/' /etc/fstab
 sudo swapoff -a
 sudo mount -a
 free -h
+echo "INSTALLING HELM"
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
+echo "UPDATING UBUNTU"
 sudo apt update
+sleep 1m
+echo "UPGRADING UBUNTU AND REBOOTING IF REQUIRED"
 sudo apt -y full-upgrade
 [ -f /var/run/reboot-required ] && sudo reboot -f
