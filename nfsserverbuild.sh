ac003d481f52830bcf8c0a3c2c77ed0a51dac386
cjmckenna <kb1sln@gmail.com>
2023-01-13 11:09:18 -0500

    Add files via upload

--- nfsserverbuild.sh
+++ nfsserverbuild.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+
+echo "DOWNLOADING"
 curl -O https://raw.githubusercontent.com/cjmckenna/kubebuilds/main/exports
-sudo cp exports /etc/exports
+echo "INSTALLING NFS"
 sudo apt install -y nfs-server
+
+echo "MAKE DIR"
 sudo mkdir /kubedata
+echo "ENABLE NFS"
 sudo systemctl enable --now nfs-server
+echo "EXPORT FS"
 sudo exportfs -ar
 
