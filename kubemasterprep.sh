5915bcdbff8e1d2b4c39229dc3e6873d20a19c41
cjmckenna <kb1sln@gmail.com>
2024-01-02 16:59:45 -0500

    Update kubemasterprep.sh

--- kubemasterprep.sh
+++ kubemasterprep.sh
@@ -9,6 +9,8 @@ curl -O https://raw.githubusercontent.com/cjmckenna/kubebuilds/main/metalLBinsta
 curl -O https://raw.githubusercontent.com/cjmckenna/kubebuilds/main/nfs-prov-install.sh && chmod 777 nfs-prov-install.sh
 curl -O https://raw.githubusercontent.com/cjmckenna/kubebuilds/main/kube-dashboard-install.sh && chmod 777 kube-dashboard-install.sh
 curl -O https://raw.githubusercontent.com/cjmckenna/kubebuilds/main/dashboard-admin-account.yaml
+curl -O https://raw.githubusercontent.com/cjmckenna/kubebuilds/main/tigera-operator.yaml
+curl -O https://raw.githubusercontent.com/cjmckenna/kubebuilds/main/custom-resources.yaml
 curl -O https://raw.githubusercontent.com/cjmckenna/kubebuilds/main/chris_sudo
 echo medic8877 | sudo -S cp chris_sudo /etc/sudoers.d/chris_sudo
 rm chris_sudo
