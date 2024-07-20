51a39454943906b88021b2764076b2287696a0ac
cjmckenna <kb1sln@gmail.com>
2024-07-20 16:51:24 -0400

    Update buildkubemaster.sh

--- buildkubemaster.sh
+++ buildkubemaster.sh
@@ -58,7 +58,7 @@ curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update
-sudo apt-get install kubelet kubeadm kubectl
+sudo apt-get -y install kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
 # Initialize master node
