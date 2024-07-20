f2fcba3d3e5d355e0ee63de377ef5e6535d25d77
cjmckenna <kb1sln@gmail.com>
2024-07-20 17:04:03 -0400

    Update calicoinstall.sh

--- calicoinstall.sh
+++ calicoinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/tigera-operator.yaml
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/custom-resources.yaml
+kubectl create -f tigera-operator.yaml
+kubectl create -f custom-resources.yaml
 echo "waiting for all pods to start up"
 watch kubectl get pods -n calico-system
 echo "Calico install completed"
