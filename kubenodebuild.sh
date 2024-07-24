ae9d351c340e06fabe5a7d855ee67099ee4fe7d3
cjmckenna <kb1sln@gmail.com>
2024-07-24 11:26:48 -0400

    Update kubenodebuild.sh

--- kubenodebuild.sh
+++ kubenodebuild.sh
@@ -1,31 +1,38 @@
 #!/bin/bash
-#!/bin/bash
 
-# Install NFS Client
-
-sudo apt install -y nfs-common
-
-#!/bin/bash
+echo "Creating keyring directory"
 
 # Create Keyring Directory
 sudo mkdir -p -m 755 /etc/apt/keyrings
 
+echo "Installing curl and apt-transport-https"
+
 sudo apt -y install curl apt-transport-https
+
+echo "Downloading public signing key"
 
 # Download public signing key
 sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
- 
+
+echo "Adding repository to APT sources"
+
 # Add the repository to Apt sources:
 # This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+echo "Installing curl, vim, git, wget, kubeadm, kubectl"
 
 sudo apt-get update
 sudo apt -y install vim git curl wget kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
+echo "Enabling kernel modules"
+
 # Enable kernel modules
 sudo modprobe overlay
 sudo modprobe br_netfilter
+
+echo "Adding sysctl settings"
 
 # Add some settings to sysctl
 sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
@@ -41,11 +48,7 @@ sudo sysctl --system
 # containerd install section
 ###############################################################################
 
-# Configure persistent loading of modules
-sudo tee /etc/modules-load.d/k8s.conf <<EOF
-overlay
-br_netfilter
-EOF
+echo "Installing Containerd"
 
 # Configure persistent loading of modules
 sudo tee /etc/modules-load.d/containerd.conf <<EOF
@@ -81,8 +84,10 @@ sudo apt install -y containerd.io
 # Configure containerd and start service
 sudo mkdir -p /etc/containerd
 sudo containerd config default|sudo tee /etc/containerd/config.toml
+sudo sed -i 's/            SystemdCgroup = false/            SystemdCgroup = true/' /etc/containerd/config.toml
 
 # restart containerd
 sudo systemctl restart containerd
 sudo systemctl enable containerd
 systemctl status  containerd
+sudo systemctl restart kubelet.service
