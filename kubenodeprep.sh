efdf849b018b8f720ca721d36dd766e2b68783ed
cjmckenna <kb1sln@gmail.com>
2024-07-24 10:26:41 -0400

    Update kubenodeprep.sh

--- kubenodeprep.sh
+++ kubenodeprep.sh
@@ -3,6 +3,10 @@
 curl -O https://raw.githubusercontent.com/cjmckenna/kubebuilds/main/kubenodebuild.sh && chmod 777 kubenodebuild.sh
 curl -O https://raw.githubusercontent.com/cjmckenna/kubebuilds/main/chris_sudo
 echo medic8877 | sudo -S cp chris_sudo /etc/sudoers.d/chris_sudo
+curl -O https://raw.githubusercontent.com/cjmckenna/kubebuilds/main/hosts
+
+sudo cp hosts /etc/hosts
+
 rm chris_sudo
 # turn off swap
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
