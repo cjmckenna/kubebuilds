8d3fb3531f1c11d86fdfbd5bd98e6a5a6f335c67
cjmckenna <kb1sln@gmail.com>
2024-12-20 18:24:42 -0500

    Update nfs-prov-install.sh

--- nfs-prov-install.sh
+++ nfs-prov-install.sh
@@ -2,7 +2,7 @@
 helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner
 helm install nfs-subdir-external-provisioner \
 nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
---set nfs.server=192.168.0.203 \
+--set nfs.server=10.101.10.5 \
 --set nfs.path=/kubedata \
 --set storageClass.onDelete=true
 kubectl get storageclass nfs-client
