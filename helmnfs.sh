67d27cfc8006a241cc21d1a7db136fd8035f2c72
cjmckenna <kb1sln@gmail.com>
2024-12-21 13:29:45 -0500

    Update helmnfs.sh

--- helmnfs.sh
+++ helmnfs.sh
@@ -4,4 +4,6 @@ nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
 --set nfs.path=/kubepvc \
 --set storageClass.onDelete=true \
 --set storageClass.name=gold
+sleep 30
+kubectl patch storageclass gold -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
 kubectl get storageclass nfs-client
