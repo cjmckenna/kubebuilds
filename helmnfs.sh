12e72bd17b61b7fcbb9921dcc008a8f4d5b09601
cjmckenna <kb1sln@gmail.com>
2024-12-21 15:17:22 -0500

    Update helmnfs.sh

--- helmnfs.sh
+++ helmnfs.sh
@@ -1,5 +1,5 @@
 helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner
-helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner --set nfs.server=10.101.10.5 --set nfs.path=/mnt/array1/kubepvc --set storageClass.onDelete=true --set storageClass.name=gold
+helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner --set nfs.server=10.101.10.5 --set nfs.path=/mnt/array1/kubepvc --set storageClass.onDelete=true --set storageClass.archiveOnDelete=false --set storageClass.name=gold
 sleep 30
 kubectl patch storageclass gold -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
 kubectl get storageclass gold
