4655159d1b3081e506999e3bd731a74ad2babc06
cjmckenna <kb1sln@gmail.com>
2024-12-21 13:35:57 -0500

    Update kube-dashboard-install.sh

--- kube-dashboard-install.sh
+++ kube-dashboard-install.sh
@@ -3,3 +3,6 @@ helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
 helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
 sleep 30
 
+kubectl patch svc kubernetes-dashboard-kong-proxy -n kubernetes-dashboard -p '{"spec": {"type": "LoadBalancer"}}'
+kubectl -n kubernetes-dashboard create token admin-user
+kubectl apply -f dashboard-admin-account.yaml
