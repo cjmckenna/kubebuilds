47beefe17b28c162d75693f12f2a48ef93470197
cjmckenna <kb1sln@gmail.com>
2024-12-21 13:39:06 -0500

    Delete nfs-prov-install.sh

--- nfs-prov-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner
-helm install nfs-subdir-external-provisioner \
-nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
---set nfs.server=10.101.10.5 \
---set nfs.path=/kubedata \
---set storageClass.onDelete=true
-kubectl get storageclass nfs-client
