c6be17230e0ae1b11bb4a3d313684956563addc2
cjmckenna <kb1sln@gmail.com>
2024-12-21 13:14:38 -0500

    Update kube-dashboard-install.sh

--- kube-dashboard-install.sh
+++ kube-dashboard-install.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
-kubectl patch service kubernetes-dashboard -p '{"spec":{"type":"LoadBalancer"}}' -n kubernetes-dashboard
-kubectl apply -f dashboard-admin-account.yaml
-kubectl -n kubernetes-dashboard create token admin-user
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
