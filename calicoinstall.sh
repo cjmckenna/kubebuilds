3c32a2f7059ad9069a48dac1643e22568dd89e82
cjmckenna <kb1sln@gmail.com>
2024-12-21 13:37:32 -0500

    Delete calicoinstall.sh

--- calicoinstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/tigera-operator.yaml
-kubectl create -f custom-resources.yaml
-echo "waiting for all pods to start up"
-watch kubectl get pods -n tigera-operator
-echo "Calico install completed"
