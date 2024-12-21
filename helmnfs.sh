91ca0a0a1208268ac254b0de2a69df508acbf06e
cjmckenna <kb1sln@gmail.com>
2024-12-21 14:39:10 -0500

    Update helmnfs.sh

--- helmnfs.sh
+++ helmnfs.sh
@@ -1,3 +1,4 @@
+helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner
 helm install nfs-subdir-external-provisioner \
 nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
 --set nfs.server=10.101.10.5 \
