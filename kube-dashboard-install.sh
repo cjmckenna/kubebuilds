51d9674324b3f546af50b06e86fff90929e88e6c
cjmckenna <kb1sln@gmail.com>
2024-12-26 16:56:36 -0500

    Update kube-dashboard-install.sh

--- kube-dashboard-install.sh
+++ kube-dashboard-install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+helm repo update
 helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
 sleep 30
 
